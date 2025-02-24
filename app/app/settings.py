fdff19a4b037934a70e1888e8b4c7372d92818da
hanoirocker <Rafael.Aguero@hearst.com>
2025-02-24 11:29:44 -0300

    llm_example: add rest_framework app to settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    #Django REST Framework
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
